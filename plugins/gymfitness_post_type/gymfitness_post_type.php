c596c37bcece3017eafd5533b439010991a965b5
Zahra <zbehfarshad@gmail.com>
2020-09-24 15:56:41 -0700

    Adding the classes Post type

--- plugins/gymfitness_post_type/gymfitness_post_type.php
+++ plugins/gymfitness_post_type/gymfitness_post_type.php
@@ -11,41 +11,41 @@
 if (!defined('ABSPATH')) die();
 
 // Registrar Custom Post Type
-function gymfitness_clases_post_type()
+function gymfitness_classes_post_type()
 {
 
 	$labels = array(
-		'name'                  => _x('Clases', 'Post Type General Name', 'gymfitness'),
-		'singular_name'         => _x('Clase', 'Post Type Singular Name', 'gymfitness'),
-		'menu_name'             => __('Clases', 'gymfitness'),
-		'name_admin_bar'        => __('Clase', 'gymfitness'),
+		'name'                  => _x('Classes', 'Post Type General Name', 'gymfitness'),
+		'singular_name'         => _x('Class', 'Post Type Singular Name', 'gymfitness'),
+		'menu_name'             => __('Classes', 'gymfitness'),
+		'name_admin_bar'        => __('Class', 'gymfitness'),
 		'archives'              => __('Archivo', 'gymfitness'),
 		'attributes'            => __('Atributos', 'gymfitness'),
-		'parent_item_colon'     => __('Clase Padre', 'gymfitness'),
-		'all_items'             => __('Todas Las Clases', 'gymfitness'),
-		'add_new_item'          => __('Agregar Clase', 'gymfitness'),
-		'add_new'               => __('Agregar Clase', 'gymfitness'),
-		'new_item'              => __('Nueva Clase', 'gymfitness'),
-		'edit_item'             => __('Editar Clase', 'gymfitness'),
-		'update_item'           => __('Actualizar Clase', 'gymfitness'),
-		'view_item'             => __('Ver Clase', 'gymfitness'),
-		'view_items'            => __('Ver Clases', 'gymfitness'),
-		'search_items'          => __('Buscar Clase', 'gymfitness'),
+		'parent_item_colon'     => __('Class Padre', 'gymfitness'),
+		'all_items'             => __('Todas Las Classes', 'gymfitness'),
+		'add_new_item'          => __('Agregar Class', 'gymfitness'),
+		'add_new'               => __('Agregar Class', 'gymfitness'),
+		'new_item'              => __('Nueva Class', 'gymfitness'),
+		'edit_item'             => __('Editar Class', 'gymfitness'),
+		'update_item'           => __('Actualizar Class', 'gymfitness'),
+		'view_item'             => __('Ver Class', 'gymfitness'),
+		'view_items'            => __('Ver Classes', 'gymfitness'),
+		'search_items'          => __('Buscar Class', 'gymfitness'),
 		'not_found'             => __('No Encontrado', 'gymfitness'),
 		'not_found_in_trash'    => __('No Encontrado en Papelera', 'gymfitness'),
 		'featured_image'        => __('Imagen Destacada', 'gymfitness'),
 		'set_featured_image'    => __('Guardar Imagen destacada', 'gymfitness'),
 		'remove_featured_image' => __('Eliminar Imagen destacada', 'gymfitness'),
 		'use_featured_image'    => __('Utilizar como Imagen Destacada', 'gymfitness'),
-		'insert_into_item'      => __('Insertar en Clase', 'gymfitness'),
-		'uploaded_to_this_item' => __('Agregado en Clase', 'gymfitness'),
-		'items_list'            => __('Lista de Clases', 'gymfitness'),
-		'items_list_navigation' => __('Navegación de Clases', 'gymfitness'),
-		'filter_items_list'     => __('Filtrar Clases', 'gymfitness'),
+		'insert_into_item'      => __('Insertar en Class', 'gymfitness'),
+		'uploaded_to_this_item' => __('Agregado en Class', 'gymfitness'),
+		'items_list'            => __('Lista de Classes', 'gymfitness'),
+		'items_list_navigation' => __('Navegación de Classes', 'gymfitness'),
+		'filter_items_list'     => __('Filtrar Classes', 'gymfitness'),
 	);
 	$args = array(
-		'label'                 => __('Clase', 'gymfitness'),
-		'description'           => __('Clases para el Sitio Web', 'gymfitness'),
+		'label'                 => __('Class', 'gymfitness'),
+		'description'           => __('Classes para el Sitio Web', 'gymfitness'),
 		'labels'                => $labels,
 		'supports'              => array('title', 'editor', 'thumbnail'),
 		'hierarchical'          => true, // true = posts , false = paginas
@@ -62,9 +62,9 @@ function gymfitness_clases_post_type()
 		'publicly_queryable'    => true,
 		'capability_type'       => 'page',
 	);
-	register_post_type('gymfitness_clases', $args);
+	register_post_type('gymfitness_Classes', $args);
 }
-add_action('init', 'gymfitness_clases_post_type', 0);
+add_action('init', 'gymfitness_classes_post_type', 0);
 
 
 // Register Custom Post Type
